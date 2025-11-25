aebba41d7ac9759199011d7da246adc2e1f0b2fa
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2025-11-25 19:22:54 -0300

    test de monitoreo creado correctamente

--- tests/monitoreo.spec.js
+++ tests/monitoreo.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://10.56.2.71:8501/");
+  await page.goto("http://localhost:8501/");
 });
 
 test.describe("Monitoreo Ambiental", () => {
@@ -15,16 +15,11 @@ test.describe("Monitoreo Ambiental", () => {
 
   test("Las metricas existen", async ({ page }) => {
     await expect(page.getByText("🌡️ Temperatura")).toBeVisible();
-    await expect(page.getByText("ºC")).toBeVisible();
     await expect(page.getByText("💧 Humedad")).toBeVisible();
-    await expect(page.getByText("%")).toBeVisible();
     await expect(page.getByText("🌫 CO₂")).toBeVisible();
-    await expect(page.getByText("ppm")).toBeVisible();
   });
 
   test("La Calidad del aire se muestra", async ({ page }) => {
     await expect(page.getByText("🟢 Bueno")).toBeVisible();
-    await expect(page.getByText("🟠 Moderado")).toBeVisible();
-    await expect(page.getByText("🔴 Malo")).toBeVisible();
   });
 });
