0bd9ef6a4f135fb89c6425373aa3ecf10337a333
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2025-12-02 20:04:15 -0300

    estuve tratando de hacer el test a los filtros de registros

--- tests/registros.spec.js
+++ tests/registros.spec.js
@@ -5,39 +5,12 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Registros Historicos", () => {
-  test("Carga la seccion de registros", async ({ page }) => {
-    await page.getByRole("heading", { name: "Registros historicos" }).click();
-    await page
-      .getByRole("heading", { name: "Datos de ETEC entre 2025-11-" })
-      .click();
-  });
-
-  test("El panel se muestra", async ({ page }) => {
-    await expect(page.getByRole("heading", { name: "Filtros" })).toBeVisible();
-    await expect(page.getByText("Selecciona la zona:")).toBeVisible();
-    await expect(page.getByText("Desde:")).toBeVisible();
-    await expect(page.getByText("Hasta:")).toBeVisible();
-  });
-
-  test("Las estadisticas se muestran", async ({ page }) => {
+  test("test", async ({ page }) => {
+    await expect(page.locator("#filtros")).toContainText("Filtros");
     await expect(
-      page.getByRole("heading", { name: "📊 Estadísticas del período" })
+      page.getByTestId("stMainBlockContainer").getByTestId("stSelectbox")
     ).toBeVisible();
-    await expect(
-      page.getByTestId("stMainBlockContainer").getByTestId("stVerticalBlock")
-    ).toContainText("Temperatura máxima:");
-    await expect(
-      page.getByTestId("stMainBlockContainer").getByTestId("stVerticalBlock")
-    ).toContainText("Temperatura promedio:");
-    await expect(
-      page.getByTestId("stMainBlockContainer").getByTestId("stVerticalBlock")
-    ).toContainText("Temperatura mínima:");
-    await expect(
-      page.getByTestId("stMainBlockContainer").getByTestId("stVerticalBlock")
-    ).toContainText("CO₂ promedio:");
-  });
-
-  test("Se grafican los datos", async ({ page }) => {
-    await expect(page.getByRole("img", { name: "0" })).toBeVisible();
+    await expect(page.getByText("Desde:Press the down arrow")).toBeVisible();
+    await expect(page.getByText("Hasta:Press the down arrow")).toBeVisible();
   });
 });
